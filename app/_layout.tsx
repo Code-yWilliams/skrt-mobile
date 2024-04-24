f8f6fdc3e72d3781739ec7a54b55be2bc43143ac
Cody Williams <cody@codywilliams.dev>
2024-04-23 20:18:14 -0700

    load custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,49 @@
-import { Slot } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { Slot } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import { setupI18n } from '~lib/i18n'
+import { Courgette_400Regular, useFonts } from '@expo-google-fonts/courgette'
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from "expo-router";
+} from 'expo-router'
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
-};
+  initialRouteName: '(tabs)',
+}
+
+setupI18n()
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const error = false;
-  const loaded = true;
+  const error = false
+
+  const [fontsLoaded] = useFonts({
+    Courgette_400Regular,
+  })
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    if (error) throw error
+  }, [error])
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
+  }, [fontsLoaded])
 
-  if (!loaded) {
-    return null;
+  if (!fontsLoaded) {
+    return null
   }
 
-  return <RootLayoutNav />;
+  return <RootLayoutNav />
 }
 
 function RootLayoutNav() {
-  return <Slot />;
+  return <Slot />
 }
