0264ee4c42b1575b79e2b912e3f8edf55488886f
Cody Williams <cody@codywilliams.dev>
2024-04-28 17:32:34 -0700

    custom query client with optional persistence

---
+++ lib/reactQuery/queryClient.tsx
@@ -0,0 +1,48 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import React from 'react'
+
+import { CURRENT_USER_KEY, AUTH_TOKEN_KEY } from './keys'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Required for persist to work properly, otherwise inactive queries will be garbage collected after the default of 5 minutes.
+      // If you don't want your query data in the memory cache forever then set your own gcTime in the useQuery options.
+      // Remember, the in-memory cache clears on app restart.
+      gcTime: Infinity,
+    },
+  },
+})
+
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+})
+
+// Only these query keys are written to async storage disk
+const KEYS_TO_PERSIST = []
+
+const QueryClientProvider = ({ children }: { children: React.ReactNode }) => (
+  <PersistQueryClientProvider
+    client={queryClient}
+    persistOptions={{
+      persister: asyncStoragePersister,
+      maxAge: Infinity,
+      dehydrateOptions: {
+        shouldDehydrateQuery: (query) => {
+          const shouldDehydrate = KEYS_TO_PERSIST.includes(
+            query.queryKey[0] as any,
+          )
+
+          return shouldDehydrate
+        },
+      },
+    }}
+  >
+    {children}
+  </PersistQueryClientProvider>
+)
+
+export { QueryClientProvider, queryClient }
