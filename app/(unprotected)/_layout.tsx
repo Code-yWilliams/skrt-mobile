f53bad4c3c588fe28cae1058e2ca65288150213f
Cody Williams <cody@codywilliams.dev>
2024-04-28 17:31:29 -0700

    navigate to home if user and token exist on login screen

--- app/(unprotected)/_layout.tsx
+++ app/(unprotected)/_layout.tsx
@@ -1,6 +1,16 @@
-import { Slot } from 'expo-router'
+import { Slot, router } from 'expo-router'
+import { useEffect } from 'react'
+import { useAuthToken, useCurrentUser } from '~lib/reactQuery/hooks/auth'
+import DeviceStorage from '~lib/utils/DeviceStorage'
 
 const Layout = () => {
+  const user = useCurrentUser()
+  const authToken = useAuthToken()
+
+  useEffect(() => {
+    if (user && authToken) router.replace('/home')
+  }, [user, authToken])
+
   return <Slot />
 }
 
