6561a4ecbb3669b2e3ed98d959dbf730305c30c9
Cody Williams <cody@codywilliams.dev>
2024-08-14 20:09:29 -0700

    remove unused mobx code

--- lib/stores/RootStore.ts
+++ lib/stores/RootStore.ts
@@ -1,12 +1,7 @@
 import { makeAutoObservable } from 'mobx'
-import UserStore from './UserStore'
 
 class RootStore {
-  userStore: UserStore
-
   constructor() {
-    this.userStore = new UserStore(this)
-
     makeAutoObservable(this)
   }
 }
